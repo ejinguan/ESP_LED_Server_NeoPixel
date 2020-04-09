1177cc1d74c72e5028a1c6e8f16128dd1aaee484
Ee Jin Guan <ejinguan@gmail.com>
2020-04-09 22:31:43 +0800

    Add animation functions (Fade, RGB Wave) (Frame and On/Off functions) and manipulation functions (direction, speed)

--- ESP_LED_Server_NeoPixel.ino
+++ ESP_LED_Server_NeoPixel.ino
@@ -33,6 +33,25 @@ float   led_hsi[3];
 #define NUM_PIXELS 24
 #define PIN_NEOPIXEL 2
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+
+// -------- LED --------------------------------
+// -------- LED Animations ---------------------
+#define anim_frame_ms 2
+unsigned long anim_last_frame = 0;  // Hold millis of last frame
+int   anim_direction = 1;           // 1 = left, 2 = right
+int   anim_speed = 5;               // 0-10
+int   anim_frame_count = 0;         // Frame counter for use
+bool  anim_fade = false;            // On/off for Fade
+bool  anim_wave = false;            // On/off for Wave             
+bool  anim_RGBwave = false;         // On/off for RGB Wave
+int   anim_brightness;              // not done yet
+
+
+// ---------------------------------------------
+// --- Handlers for various endpoints ----------
+// ---------------------------------------------
+
+// --- Generic handler to generate homepage ----
 void handleHomePage () {
 
   String buf;
@@ -41,29 +60,62 @@ void handleHomePage () {
   buf = buf + "<meta charset=\"utf-8\">";
   buf = buf + "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">";
   buf = buf + "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
-  buf = buf + "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\">";
+  buf = buf + "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\">"; // 3.3.6
   buf = buf + "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js\"></script>";
   buf = buf + "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jscolor/2.0.4/jscolor.min.js\"></script>";
   buf = buf + "</head><body><div class=\"container\"><div class=\"row\"><h1>ESP Color Picker</h1>";
-  buf = buf + "<a type=\"submit\" id=\"change_color\" type=\"button\" class=\"btn btn-primary\">Change Color</a> ";
-  buf = buf + "<input class=\"jscolor {onFineChange:'update(this)'}\" id=\"rgb\" value=\"" + led_rgb + "\"></div></div>";
+  buf = buf + "<div class=\"input-group\">";
+  buf = buf + "<a type=\"submit\" id=\"change_color\" type=\"button\" class=\"btn btn-primary input-group-addon\">Change Color</a>";
+  buf = buf + "<input class=\"form-control jscolor {onFineChange:'update(this)'} form-control\" id=\"rgb\" value=\"" + led_rgb + "\"></div></div>";
   buf = buf + "<script>function update(picker) {document.getElementById('rgb').innerHTML = Math.round(picker.rgb[0]) + ', ' +  Math.round(picker.rgb[1]) + ', ' + Math.round(picker.rgb[2]);";
-  buf = buf + "document.getElementById(\"change_color\").href=document.getElementById('rgb').value;}</script></body></html>";
+  buf = buf + "document.getElementById(\"change_color\").href=document.getElementById('rgb').value;}</script>";
+  buf = buf + "<div class=\"row\">HSI: H " + led_hsi[0] + " S " + led_hsi[1] + " I " + led_hsi[2] + "</div>";
+  buf = buf + "<div class=\"row\">&nbsp;</div>";
+  buf = buf + "<div class=\"row\">Fade: " + (anim_fade? "<b>On</b> | <a href=\"../fade/0\">Off</a>" : "<a href=\"../fade/1\">On</a> | <b>Off</b>") + "</div>";
+  buf = buf + "<div class=\"row\">RGB Wave: " + (anim_RGBwave? "<b>On</b> | <a href=\"../rgbwave/0\">Off</a>" : "<a href=\"../rgbwave/1\">On</a> | <b>Off</b>") + "</div>";
+  buf = buf + "<div class=\"row\">Speed: ";
+  for (int i = 0; i<=10; i++) {
+    if (i==anim_speed){
+      buf = buf + "<b>" + i + "</b> ";
+    } else {
+      buf = buf + "<a href=\"../speed/" + i + "\">" + i + "</a> ";
+    }
+  }
+  buf = buf + "</div>";
+  buf = buf + "<div class=\"row\">Direction: ";
+  for (int i = 1; i<=4; i++) {
+    if (i==anim_direction){
+      buf = buf + "<b>" + i + "</b> ";
+    } else {
+      buf = buf + "<a href=\"../dir/" + i + "\">" + i + "</a> ";
+    }
+  }
+  buf = buf + "</div>";
+  buf = buf + "<div class=\"row\">";
+  buf = buf +   "Anim_Frame_Count: " + anim_frame_count;
+  buf = buf + "</div>";
+  buf = buf + "</div></body></html>";
   
-  server.send(200, "text/HTML", buf);  
+  server.send(200, "text/HTML", buf);
 }
 
+
+// --- Handle root directory -------------------
 void handleRoot() {
-  server.sendHeader("Location","/LED/");        // Add a header to respond with a new location for the browser to go to the home page again
+  
+  // Force a redirect to /LED
+  server.sendHeader("Location","/LED/");    // Add a header to respond with a new location for the browser to go to the home page again
   server.send(303);                         // Send it back to the browser with an HTTP status 303 (See Other) to redirect
   
-  
-  //digitalWrite(led, 0);
+  // Optionally can send HTTP response
   //server.send(200, "text/plain", "hello from esp8266!");
-  //digitalWrite(led, 1);
 }
 
+
+// --- Handle where path was not configured ----
+// Here catch if it's a case where RGB code is specified
 void handleNotFound() {
+  
   // Check if it's a request for LED
   String my_url;
   my_url = server.uri();
@@ -71,6 +123,18 @@ void handleNotFound() {
   
   if (my_url.startsWith("/LED")) {
     handleLED();
+    
+  } else if (my_url.startsWith("/FADE")) {
+    handleFade();
+
+  } else if (my_url.startsWith("/RGBWAVE")) {
+    handleRGBWave();
+
+  } else if (my_url.startsWith("/SPEED")) {
+    handleSpeed();
+
+  } else if (my_url.startsWith("/DIR")) {
+    handleDirection();
     
   } else {
     String message = "File Not Found\n\n";
@@ -89,6 +153,9 @@ void handleNotFound() {
   }
 }
 
+
+// --- Handle LED RGB input --------------------
+// Here catch if it's a case where RGB code is specified
 void handleLED() {
 
   String my_url;
@@ -109,7 +176,8 @@ void handleLED() {
   // Process the url if it is 6 char (assume hex string)
   if (led_param.length() == 6) {
 
-    setColor(led_param);
+    // If different then set colour
+    if (led_rgb != led_param) setColor(led_param);
   
     handleHomePage();
   } else {
@@ -117,12 +185,19 @@ void handleLED() {
   }
 }
 
-void setColor(String rgb) {
-  int red_value   = (rgb.charAt(0) - '0')*16 + (rgb.charAt(1) - '0');
-  int green_value = (rgb.charAt(2) - '0')*16 + (rgb.charAt(3) - '0');
-  int blue_value  = (rgb.charAt(4) - '0')*16 + (rgb.charAt(5) - '0');
 
-  for(uint16_t i=0; i< strip.numPixels(); i++) {
+void setColor(String rgb) {
+  int red_value   = hex_2_int(rgb.charAt(0))*16 + hex_2_int(rgb.charAt(1));
+  int green_value = hex_2_int(rgb.charAt(2))*16 + hex_2_int(rgb.charAt(3));
+  int blue_value  = hex_2_int(rgb.charAt(4))*16 + hex_2_int(rgb.charAt(5));
+  
+  led_colors[0] = red_value;
+  led_colors[1] = green_value;
+  led_colors[2] = blue_value;
+
+  rgb2hsi(red_value, green_value, blue_value, led_hsi);
+
+  for(uint16_t i=0; i < NUM_PIXELS; i++) {
     strip.setPixelColor(i, strip.Color(red_value, green_value, blue_value));
     //delay(50);
     strip.show();
@@ -132,13 +207,169 @@ void setColor(String rgb) {
 }
 
 
+void handleSpeed() {
+
+  String my_url;
+  my_url = server.uri();
+  my_url.toUpperCase();  
+  // url will look like "/SPEED/xxxxxx"
+  anim_speed = my_url.substring(7).toInt();
+  
+  if (anim_speed < 0) anim_speed = 0;
+  if (anim_speed > 10) anim_speed = 10;
+
+  // Cause a redirect back to /LED/
+  handleRoot(); 
+  
+}
+
+void handleDirection() {
+
+  String my_url;
+  my_url = server.uri();
+  my_url.toUpperCase();  
+  // url will look like "/DIR/xxxxxx"
+  anim_direction = my_url.substring(5).toInt();
+  
+  if (anim_direction < 1) anim_direction = 1;
+  if (anim_direction > 4) anim_direction = 4;
+
+  // Cause a redirect back to /LED/
+  handleRoot(); 
+  
+}
+
+
+
+// Fade in and out 
+void handleFade() {
+
+  String my_url;
+  my_url = server.uri();
+  my_url.toUpperCase();  
+  // url will look like "/fade/xxxxxx"
+  String my_string = my_url.substring(6);
+
+  if (my_string.length() == 0) {
+    anim_fade = !anim_fade;
+  } else {
+    anim_fade = my_string.toInt();
+  }
+  if (anim_fade) {
+    anim_frame_count = 0;
+  } else {
+    setColor(led_rgb);
+  }
+  
+  // Cause a redirect back to /LED/
+  handleRoot(); 
+}
+void FadeFrame() {
+  if (anim_speed==0) return;
+  
+  int red_value   = led_colors[0];
+  int green_value = led_colors[1];
+  int blue_value  = led_colors[2];
+
+  int breakpoint = 2 * 1000 / anim_frame_ms / anim_speed;
+  int anim_frame = anim_frame_count % breakpoint;
+
+  if ((anim_frame_count / breakpoint) % 2 == 1) {
+    // increasing brightness
+    red_value   = (red_value * anim_frame) / breakpoint;
+    green_value = (green_value * anim_frame) / breakpoint;
+    blue_value  = (blue_value * anim_frame) / breakpoint;
+  } else {
+    // decreasing brightness
+    red_value   = red_value - (red_value * anim_frame) / breakpoint;
+    green_value = green_value - (green_value * anim_frame) / breakpoint;
+    blue_value  = blue_value - (blue_value * anim_frame) / breakpoint;
+  }
+  
+  Serial.print(red_value);
+  Serial.print(" "); 
+  Serial.print(green_value); 
+  Serial.print(" ");
+  Serial.println(blue_value);
+  
+  for(uint16_t i=0; i< strip.numPixels(); i++) {
+    strip.setPixelColor(i, strip.Color(red_value, green_value, blue_value));
+    strip.show();
+  }
+}
+
+// Fade wave across the board
+void handleWave() {
+
+  
+}
+void WaveFrame() {
+  
+}
+
+// RGB wave across the board
+void handleRGBWave() {
+
+  String my_url;
+  my_url = server.uri();
+  my_url.toUpperCase();  
+  // url will look like "/rgbwave/xxxxxx"
+  String my_string = my_url.substring(9);
+
+  if (my_string.length() == 0) {
+    anim_RGBwave = !anim_RGBwave;
+  } else {
+    anim_RGBwave = my_string.toInt();
+  }
+  if (anim_RGBwave) {
+    anim_frame_count = 0;
+  } else {
+    setColor(led_rgb);
+  }
+  
+  // Cause a redirect back to /LED/
+  handleRoot(); 
+  
+}
+void RGBWaveFrame() {
+  if (anim_speed==0) return;
+
+  // To store temporary RGB results
+  int led_temp[3];
+
+  uint16_t hue_increment = 360 / NUM_PIXELS;
+
+  int breakpoint = 2 * 1000 / anim_frame_ms / anim_speed;
+  int anim_frame = anim_frame_count % breakpoint;
+  
+
+  for(uint16_t i=0; i < NUM_PIXELS; i++) {
+    // Add hue increment to each LED
+
+    
+    hsi2rgb(led_hsi[0] + (anim_direction%2 == 1?-1:1) * i*hue_increment                    // 1,2 directions -1, 3,4 directions 1
+                       + (anim_direction <= 2?-1:1) * (360.0 * anim_frame/breakpoint), // odd directions -1, even directions 1
+            led_hsi[1], 
+            led_hsi[2], 
+            led_temp);
+    
+    strip.setPixelColor(i, strip.Color(led_temp[0], led_temp[1], led_temp[2]));
+    //delay(50);
+    strip.show();
+  }
+
+  
+}
+
+
 void setup(void) {
+  // Set Neopixel pin as output
   pinMode(PIN_NEOPIXEL, OUTPUT);
   
   setColor("110000"); //red
   Serial.begin(115200);
 
-  delay(1000); // Prevent crashing on boot
+  delay(1000); // Prevent drawing too much current with LED and crash on boot
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("");
@@ -198,6 +429,27 @@ void setup(void) {
 void loop(void) {
   server.handleClient();
   MDNS.update();
+
+  // Check if current millis is more than 1 frame from last
+  if (millis() > anim_last_frame + anim_frame_ms) {
+    // Do a frame update
+    if (anim_fade) {
+      FadeFrame();
+    }
+    if (anim_wave) {
+      WaveFrame();
+    }
+    if (anim_RGBwave) {
+      RGBWaveFrame();
+    }
+
+    anim_last_frame = millis();
+    if (anim_speed > 0) anim_frame_count++;
+  }
+}
+
+
+
 // From: http://blog.saikoled.com/post/43693602826/why-every-led-light-should-be-using-hsi
 //
 // Function example takes H, S, I, and a pointer to the 
